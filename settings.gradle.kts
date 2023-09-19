026dffd28dc4935ee254861f82c09f84ea088f6e
Rupak <rupakdas.dev@gmail.com>
2023-09-19 23:07:11 +0530

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Gym_Rat"
+include(":androidApp")
+include(":shared")
